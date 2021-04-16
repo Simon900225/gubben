3dff56496860b56be7382a21a0ca64a9fa5e309a
Simon Bengtsson <simon900225@gmail.com>
2021-04-16 17:22:52 +0200

    Added help command and restart valheim command

--- DiscordGubbBot/Modules/PublicModule.cs
+++ DiscordGubbBot/Modules/PublicModule.cs
@@ -1,16 +1,82 @@
+using System;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using DiscordGubbBot.Services;
+using Microsoft.Extensions.Configuration;
+using Renci.SshNet;
 
 namespace DiscordGubbBot.Modules
 {
     // Modules must be public and inherit from an IModuleBase
     public class PublicModule : ModuleBase<SocketCommandContext>
     {
+        private readonly string Password = Environment.GetEnvironmentVariable("sshPassword");
+        private readonly string Username = Environment.GetEnvironmentVariable("sshUsername");
+        private readonly string Host = Environment.GetEnvironmentVariable("sshUrl");
+
         // Dependency Injection will fill this value in for us
         public PictureService PictureService { get; set; }
+        //public IConfiguration Configuration { get; set; } //Crashes application?
+
+
+        //TODO; Add info attribute that explains the command.
+        [Command("vad kan du göra?")]
+        [Alias("help", "hjälp")]
+        public async Task CanDoInfo()
+        {
+            StringBuilder sb = new();
+            int i = 1;
+            foreach (var method in typeof(PublicModule).GetMethods())
+            {
+                var commandAttributes = method.GetCustomAttributes().Where(x => x.GetType() == typeof(CommandAttribute)).ToList();
+                var aliasAttributes = method.GetCustomAttributes().Where(x => x.GetType() == typeof(AliasAttribute)).ToList();
+                if (commandAttributes.Count > 0)
+                {
+                    sb.Append($"{i}. '");
+
+                    sb.Append(string.Join("', '", commandAttributes.Cast<CommandAttribute>().Select(x => x.Text)));
+
+                    if (aliasAttributes.Count > 0)
+                        sb.Append("', '");
+
+                    sb.Append(string.Join("', '", aliasAttributes.Cast<AliasAttribute>().SelectMany(x => x.Aliases).Select(x => x.ToString())));
+                    sb.Append("'\n");
+                    i++;
+                }
+
+
+            }
+            await ReplyAsync(sb.ToString());
+        }
+
+        [Command("update valheim")]
+        [Alias("uppdatera valheim", "starta om valheim", "restart valheim")]
+        public async Task UpdateValheim()
+        {
+            var ConnNfo = new ConnectionInfo(Host, 22, Username,
+                new AuthenticationMethod[]{
+                    new PasswordAuthenticationMethod(Username, Password),
+                }
+            );
+
+            using (var sshclient = new SshClient(ConnNfo))
+            {
+                sshclient.Connect();
+                using var stream = sshclient.CreateShellStream("xterm", 255, 50, 800, 600, 1024);
+                stream.Write("sudo docker restart dockercomposes_valheim_1\n");
+                stream.Expect($"password for {Username}:");
+                stream.Write(Password + "\n");
+
+                sshclient.Disconnect();
+            }
+
+            await ReplyAsync("I fix. Vänta ca 2 min så ska servern vara igång igen.");
+        }
 
         [Command("dox")]
         public async Task DoxUserAsync(IUser user = null)
